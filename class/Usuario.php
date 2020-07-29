b94c25ca79034bd0d92d6b80f95c1b0c8ad63d13
unknown <eribertotorres@gmail.com>
2020-07-29 15:35:15 -0300

    inserir e alterar

--- class/Usuario.php
+++ class/Usuario.php
@@ -38,23 +38,18 @@ class Usuario {
 	public function setDtcadastro($value){
 		$this->dtcadastro = $value;
 	}
-
+	
 	public function loadById($id){
 
 		$sql = new Sql();
 
-		$results = $sql->select("SELECT * FROM tb_usuarios WHERE  idusuario = :ID", array(
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
 			":ID"=>$id
-	));
+		));
 
-		if (count($results) > 0 ) {
+		if (count($results) > 0) {
 
-			$row = $results[0];
-
-			$this->setIdusuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+			$this->setData($results[0]);
 
 		}
 
@@ -65,7 +60,9 @@ class Usuario {
 		$sql = new Sql();
 
 		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+
 	}
+
 	public static function search($login){
 
 		$sql = new Sql();
@@ -80,25 +77,81 @@ class Usuario {
 
 		$sql = new Sql();
 
-		$results = $sql->select("SELECT * FROM tb_usuarios WHERE  deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
 			":LOGIN"=>$login,
 			":PASSWORD"=>$password
-	));
+		));
 
-		if (count($results) > 0 ) {
+		if (count($results) > 0) {
 
-			$row = $results[0];
-
-			$this->setIdusuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+			$this->setData($results[0]);
 
 		} else {
 
-			throw new Exception ("Login e/ou senha inválidos");
+			throw new Exception("Login e/ou senha inválidos.");
+
 		}
 
+	}
+
+	public function setData($data){
+
+		$this->setIdusuario($data['idusuario']);
+		$this->setDeslogin($data['deslogin']);
+		$this->setDessenha($data['dessenha']);
+		$this->setDtcadastro(new DateTime($data['dtcadastro']));
+
+	}
+
+	public function insert(){
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+			':LOGIN'=>$this->getDeslogin(),
+			':PASSWORD'=>$this->getDessenha()
+		));
+
+		if (count($results) > 0) {
+			$this->setData($results[0]);
+		}
+
+	}
+
+	public function update($login, $password){
+
+		$this->setDeslogin($login);
+		$this->setDessenha($password);
+
+		$sql = new Sql();
+
+		$sql->query("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID", array(
+			':LOGIN'=>$this->getDeslogin(),
+			':PASSWORD'=>$this->getDessenha(),
+			':ID'=>$this->getIdusuario()
+		));
+
+	}
+
+	public function delete(){
+
+		$sql = new Sql();
+
+		$sql->query("DELETE FROM tb_usuarios WHERE idusuario = :ID", array(
+			':ID'=>$this->getIdusuario()
+		));
+
+		$this->setIdusuario(0);
+		$this->setDeslogin("");
+		$this->setDessenha("");
+		$this->setDtcadastro(new DateTime());
+
+	}
+
+	public function __construct($login = "", $password = ""){
+
+		$this->setDeslogin($login);
+		$this->setDessenha($password);
 
 	}
 
@@ -108,8 +161,13 @@ class Usuario {
 			"idusuario"=>$this->getIdusuario(),
 			"deslogin"=>$this->getDeslogin(),
 			"dessenha"=>$this->getDessenha(),
-			"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
+			"dtcadastro"=>$this->getDtcadastro()->format("d-m-Y H:i:s")
 		));
+
 	}
-}
-?>
+
+} 	
+	
+
+
+ ?>
--- index.php
+++ index.php
@@ -1,26 +1,45 @@
-<?php
+<?php 
 
 require_once("config.php");
 
-/* carrega um usuário
-$root = new Usuario();
-$root->loadByID(3);
-echo $root;
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadbyId(3);
+//echo $root;
+
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//carrega um usuário usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("root", "!@#$");
+
+//echo $usuario;
+
+//Criando um novo usuário
+/*
+$aluno = new Usuario("aluno", "@lun0");
+$aluno->insert();
+echo $aluno;
 */
 
-/*Carrega uma lista de usuários
-$lista = Usuario::getList();
-echo json_encode($lista):*/
 
-/*Carrega uma lista de usuarios buscando pelo login
-$search = Usuario::search("jo");
-echo json_encode($search);*/
-
-//Carrega um usuário usando o login e a senha
-
+//Alterar um usuário
 $usuario = new Usuario();
-$usuario->login("jose","1234567890");
-
+$usuario->loadById(1);
+$usuario->update("professor", "!@#$%¨&*");
 echo $usuario;
 
-?>
+
+/*
+$usuario = new Usuario();
+$usuario->loadById(7);
+$usuario->delete();
+echo $usuario;
+*/
+ ?>
